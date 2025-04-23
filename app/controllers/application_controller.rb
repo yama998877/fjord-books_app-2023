f4e35d79acc4b6a873a7079c9334b3d89af8dfb4
yama998877 <yamada.sss.369@gmail.com>
2025-04-23 20:13:50 +0900

    ユーザー認証機能、ユーザーページ、ユーザーリストを実装
    郵便番号、住所、自己紹介のデータカラムを追加。
    郵便番号のバリデーションを実装。
    ログイン、ログアウト、ユーザーデータ更新後の移動先ページを変更。
    ユーザー編集ページに郵便番号、住所、自己紹介を追加。
    ユーザー詳細ページを追加。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action do
+    I18n.locale = :ja
+  end
+
+  # ログイン後、本の一覧ページに移動
+  def after_sign_in_path_for(_resource)
+    books_path
+  end
+
+  # ログアウト後、ログインページに移動
+  def after_sign_out_path_for(_resource)
+    new_user_session_path
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  validates :postal_code, length: { is: 7 }, on: :update, allow_blank: true,
+                          format: { with: /\A[0-9]+\z/, message: 'は半角数字のみが使えます' }
+end
