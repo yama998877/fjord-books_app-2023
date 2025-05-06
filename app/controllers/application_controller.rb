2d0f30660883ed5c98ce0f55f515a15d49f1f39e
yama998877 <yamada.sss.369@gmail.com>
2025-05-06 21:37:01 +0900

    configure_permitted_parametersメソッドを追加。
    RegistrationController#updateで削除したコードをconfigure_permitted_parametersメソッドとしてbefore_actionで呼び出して利用。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action do
     I18n.locale = :ja
   end
@@ -14,5 +15,11 @@ class ApplicationController < ActionController::Base
   # ログアウト後、ログインページに移動
   def after_sign_out_path_for(_resource)
     new_user_session_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address self_introduction])
   end
 end
