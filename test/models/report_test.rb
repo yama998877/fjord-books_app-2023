5c38c250bc158378933b6a1770f4f10b17df78d6
yama998877 <yamada.sss.369@gmail.com>
2026-04-23 22:06:14 +0900

    reportとuserのモデルテスト、日報機能のシステムテストを実装
    reportとuserのテストデータを作成
    reportとuserのモデルメソッドのテストを実装
    日報機能のシステムテスト(一覧ページのチェック、作成時刻の表示チェック、新規作成、更新、削除のチェック)実装

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,24 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?(target_user)' do
+    report = reports(:alice_report)
+    user = users(:alice)
+
+    assert_equal true, report.editable?(user)
+  end
+  test '#created_on' do
+    report = reports(:alice_report)
+    assert_equal report.created_at.to_date, report.created_on
+  end
+  test '#save_mentions' do
+    user1 = users(:alice)
+    user2 = users(:bob)
+    report1 = user1.reports.create!(title: '日報', content: 'これからよろしくお願いします。')
+    report2 = user2.reports.create!(title: '日報', content: "http://localhost:3000/reports/#{report1.id}が参考になりました")
+    assert_equal [report1], report2.mentioning_reports
+
+    report2.update(title: '更新', content: '更新しました。')
+    assert_equal [], report2.reload.mentioning_reports
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#name_or_email' do
+    users = [users(:alice), users(:bob)]
+    assert_equal 'Alice', users[0].name_or_email
+    assert_equal 'bobob@example.com', users[1].name_or_email
+  end
 end
